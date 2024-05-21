f48b6fa3d12c2a28c90b68fde2ab4d8e0cef2f32
GermanGric <gric427@gmail.com>
2024-05-21 23:02:09 +0300

    новый контроллер под бд и обновленый файл продукт

--- src/main/java/com/example/OnlineStore/models/Product.java
+++ src/main/java/com/example/OnlineStore/models/Product.java
@@ -1,18 +1,33 @@
 package com.example.OnlineStore.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
 
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "products")
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
     private Long id;
+    @Column(name = "title")
     private String title;
+    @Column(name = "description", columnDefinition = "text")
     private String description;
+    @Column(name = "price")
     private int price;
+    @Column(name = "city")
     private String city;
+    @Column(name = "author")
     private String author;
-
 }
